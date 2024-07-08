09f70d71ec802e799a057cc859f84737a68f9fe9
Hyoseop Song <crad_on25@naver.com>
2024-07-08 20:50:30 +0900

    🎉 Init: set up project

---
+++ src/main/java/com/notitime/noffice/NofficeApplication.java
@@ -0,0 +1,13 @@
+package com.notitime.noffice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NofficeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NofficeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/notitime/noffice/NofficeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.notitime.noffice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NofficeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
