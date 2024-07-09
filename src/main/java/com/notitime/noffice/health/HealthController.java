8a9651bb4070f5c0fdf99c3f3503465581509014
Hyoseop Song <crad_on25@naver.com>
2024-07-09 19:24:44 +0900

    :sparkles: feat: Health API 추가

---
+++ src/main/java/com/notitime/noffice/health/HealthController.java
@@ -0,0 +1,12 @@
+package com.notitime.noffice.health;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthController {
+	@GetMapping("/health")
+	public String health() {
+		return "OK";
+	}
+}
