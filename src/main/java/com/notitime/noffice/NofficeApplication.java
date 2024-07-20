e375175ae2ea2f5f5b2122a358eb6b67ae4013f1
Hyoseop Song <crad_on25@naver.com>
2024-07-20 15:21:29 +0900

    :sparkles: feat: BaseTimeEntity 추가 BaseTimeEntity 도메인 생성 및 수정 시점을 부여한다.

--- src/main/java/com/notitime/noffice/NofficeApplication.java
+++ src/main/java/com/notitime/noffice/NofficeApplication.java
@@ -2,8 +2,10 @@ package com.notitime.noffice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class NofficeApplication {
 
 	public static void main(String[] args) {
