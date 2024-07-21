feba135c8309124025644f5e9997ceca8f1569ff
Hyoseop Song <crad_on25@naver.com>
2024-07-21 16:21:28 +0900

    :sparkles: feat: 응답 advice 구현체 대상 패키지 지정

--- src/main/java/com/notitime/noffice/global/advice/NofficeResponseAdvice.java
+++ src/main/java/com/notitime/noffice/global/advice/NofficeResponseAdvice.java
@@ -7,8 +7,10 @@ import org.springframework.http.MediaType;
 import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.http.server.ServerHttpRequest;
 import org.springframework.http.server.ServerHttpResponse;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseBodyAdvice;
 
+@RestControllerAdvice(basePackages = "com.notitime.noffice")
 public class NofficeResponseAdvice implements ResponseBodyAdvice<Object> {
 	@Override
 	public boolean supports(MethodParameter returnType, Class<? extends HttpMessageConverter<?>> converterType) {
