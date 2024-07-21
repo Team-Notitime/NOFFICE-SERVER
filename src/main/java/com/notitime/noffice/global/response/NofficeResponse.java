1b3672166186e3c7dcc01223a71f2491415191c1
Hyoseop Song <crad_on25@naver.com>
2024-07-21 16:15:24 +0900

    :sparkles: feat: 공통 비즈니스 응답 형식 구현

---
+++ src/main/java/com/notitime/noffice/global/response/NofficeResponse.java
@@ -0,0 +1,57 @@
+package com.notitime.noffice.global.response;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonInclude.Include;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+import java.time.LocalDateTime;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@JsonPropertyOrder({"timestamp", "httpStatus", "code", "message", "data"})
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@Builder(access = AccessLevel.PRIVATE)
+public class NofficeResponse<T> {
+
+	@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy.MM.dd HH:mm:ss", timezone = "Asia/Seoul")
+	@JsonProperty("timestamp")
+	private LocalDateTime timestamp = LocalDateTime.now();
+
+	private final int httpStatus;
+
+	private final String code;
+
+	private final String message;
+
+	@JsonInclude(value = Include.NON_NULL)
+	private T data;
+
+	public static <T> NofficeResponse<T> success(SuccessCode success) {
+		return new NofficeResponse<>(success.getHttpStatus().value(), success.getCode(), success.getMessage());
+	}
+
+	public static <T> NofficeResponse<T> success(SuccessCode success, T data) {
+		return new NofficeResponse<>(success.getHttpStatus().value(), success.getCode(), success.getMessage(), data);
+	}
+
+	public static <T> NofficeResponse<T> fail(ErrorCode error) {
+		return new NofficeResponse<>(error.getHttpStatus().value(), error.getCode(), error.getMessage());
+	}
+
+	NofficeResponse(int httpStatus, String code, String message) {
+		this.httpStatus = httpStatus;
+		this.code = code;
+		this.message = message;
+	}
+
+	NofficeResponse(int httpStatus, String code, String message, T data) {
+		this.httpStatus = httpStatus;
+		this.code = code;
+		this.message = message;
+		this.data = data;
+	}
+}
