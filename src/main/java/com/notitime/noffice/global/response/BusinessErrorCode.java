e26114277240586143fdd0b6dc1c70126ac85ed6
Hyoseop Song <crad_on25@naver.com>
2024-07-27 07:29:45 +0900

    :construction: chore: 인증 예외 케이스 상태코드 부여

--- src/main/java/com/notitime/noffice/global/response/BusinessErrorCode.java
+++ src/main/java/com/notitime/noffice/global/response/BusinessErrorCode.java
@@ -9,7 +9,13 @@ import org.springframework.http.HttpStatus;
 public enum BusinessErrorCode implements ErrorCode {
 
 	BAD_REQUEST(HttpStatus.BAD_REQUEST, "NOF-400", "잘못된 요청입니다."),
-	INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "NOF-500", "서버 내부 오류가 발생했습니다.");
+	FAILED_TO_LOAD_PRIVATE_KEY(HttpStatus.BAD_REQUEST, "NOF-400", "개인 키를 로드하는 데 실패했습니다."),
+	INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "NOF-500", "서버 내부 오류가 발생했습니다."),
+	INVALID_APPLE_IDENTITY_TOKEN(HttpStatus.UNAUTHORIZED, "NOF-401", "유효하지 않은 아이덴티티 토큰입니다."),
+	UNSUPPORTED_ALGORITHM(HttpStatus.BAD_REQUEST, "NOF-400", "키 생성에 사용된 알고리즘을 지원하지 않습니다: "),
+	INVALID_KEY_SPEC(HttpStatus.BAD_REQUEST, "NOF-400", "공개 키 생성에 잘못된 키 사양이 제공되었습니다."),
+	UNAUTHORIZED(HttpStatus.UNAUTHORIZED, "NOF-401", "인증되지 않은 사용자입니다.");
+
 
 	private final HttpStatus httpStatus;
 	private final String code;
