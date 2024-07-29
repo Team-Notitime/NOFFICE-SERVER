1e4a84d541fd32705f7b7cf1e39b1de7567f6a7f
Hyoseop Song <crad_on25@naver.com>
2024-07-29 20:06:29 +0900

    :fire: fix: 인가 서버 리프레시 토큰 재발행 메소드 제거
    - 로그아웃, 서비스 탈퇴 요청 시 사용자 검증을 위해 사용되나, 미구현 기능이므로 제거

--- src/main/java/com/notitime/noffice/external/openfeign/apple/AppleOAuthProvider.java
+++ src/main/java/com/notitime/noffice/external/openfeign/apple/AppleOAuthProvider.java
@@ -1,10 +1,7 @@
 package com.notitime.noffice.external.openfeign.apple;
 
 import com.notitime.noffice.external.openfeign.apple.dto.ApplePublicKeys;
-import com.notitime.noffice.external.openfeign.apple.dto.AppleTokenResponse;
 import com.notitime.noffice.external.openfeign.dto.AuthorizedMemberInfo;
-import com.notitime.noffice.global.exception.BadRequestException;
-import com.notitime.noffice.global.response.BusinessErrorCode;
 import io.jsonwebtoken.Claims;
 import java.security.PublicKey;
 import java.util.Map;
@@ -25,31 +22,14 @@ public class AppleOAuthProvider {
 	@Value("${oauth.apple.client-id}")
 	private String clientId;
 
-	public AuthorizedMemberInfo getAppleUserInfo(final String identityToken, final String name) {
+	public AuthorizedMemberInfo getAppleUserInfo(final String identityToken, final String name,
+	                                             ApplePublicKeys applePublicKeys) {
 		Map<String, String> headers = appleIdentityTokenParser.parseHeaders(identityToken);
-		ApplePublicKeys applePublicKeys = appleFeignClient.getApplePublicKey();
 		PublicKey applePublicKey = applePublicKeyGenerator.generatePublicKey(headers, applePublicKeys);
 		Claims claims = appleIdentityTokenParser.parsePublicKeyAndGetClaims(identityToken, applePublicKey);
 		return AuthorizedMemberInfo.of(
 				claims.get("sub").toString(),
 				name,
 				claims.get("email").toString());
-	}
-
-	public String getAppleRefreshToken(final String code, final String clientSecret) {
-		try {
-			AppleTokenResponse appleTokenResponse = appleFeignClient.getAppleTokens(code, clientId, clientSecret,
-					"authorization_code");
-			log.info("Apple token response: {}", appleTokenResponse);
-			return appleTokenResponse.refreshToken();
-		} catch (Exception e) {
-			log.error("Failed to get apple refresh token.");
-			throw new BadRequestException(BusinessErrorCode.FAILED_TO_LOAD_PRIVATE_KEY);
-		}
-	}
-
-	public void requestRevoke(final String refreshToken, final String clientSecret) {
-		appleFeignClient.revoke(refreshToken, clientId, clientSecret, "refresh_token");
-		log.error("Failed to revoke apple refresh token.");
 	}
 }
