dc24c72054bebeb951db97a2418885d2ce5a7d0c
Hyoseop Song <crad_on25@naver.com>
2024-07-30 16:21:38 +0900

    :construction: fix: 게스트 사용자 테스트를 위한 api security 제한 임시해제
    - TODO : 인증된 사용자를 통한 요청 구축시 whitelist 제약 재설정

--- src/main/java/com/notitime/noffice/global/config/SecurityConfig.java
+++ src/main/java/com/notitime/noffice/global/config/SecurityConfig.java
@@ -6,12 +6,16 @@ import com.notitime.noffice.auth.filter.JwtAuthenticationFilter;
 import com.notitime.noffice.auth.jwt.JwtProvider;
 import com.notitime.noffice.auth.jwt.JwtValidator;
 import lombok.RequiredArgsConstructor;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer.FrameOptionsConfig;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
@@ -24,8 +28,8 @@ public class SecurityConfig {
 	private final JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint;
 	private final JwtValidator jwtValidator;
 	private final JwtProvider jwtProvider;
-	private static final String[] whiteList = {"/api/v1/member/login", "/api/v1/member/reissue",
-			"/health", "/swagger-ui/**", "/swagger-resources/**", "/v3/api-docs/**", "/webjars/**"};
+	private static final String[] whiteList = {"/api/v1/**",
+			"/health", "/swagger-ui/**", "/swagger-resources/**", "/v3/api-docs/**", "/webjars/**", "/h2-console/**"};
 
 	@Bean
 	@Order(2)
