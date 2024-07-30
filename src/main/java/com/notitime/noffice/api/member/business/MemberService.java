704467239e1974b2349830e9f8c93b77bfed4ed4
Hyoseop Song <crad_on25@naver.com>
2024-07-30 16:23:28 +0900

    :sparkles: feat: 단일 멤버 조회 기능 구현 (#28)

--- src/main/java/com/notitime/noffice/api/member/business/MemberService.java
+++ src/main/java/com/notitime/noffice/api/member/business/MemberService.java
@@ -1,4 +1,28 @@
 package com.notitime.noffice.api.member.business;
 
+import com.notitime.noffice.domain.member.model.Member;
+import com.notitime.noffice.domain.member.persistence.MemberRepository;
+import com.notitime.noffice.global.exception.NotFoundException;
+import com.notitime.noffice.global.response.BusinessErrorCode;
+import com.notitime.noffice.response.MemberResponse;
+import com.notitime.noffice.response.OrganizationResponses;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional
+@RequiredArgsConstructor
 public class MemberService {
+	private final MemberRepository memberRepository;
+
+	public MemberResponse getMember(Long memberId) {
+		Member member = memberRepository.findById(memberId)
+				.orElseThrow(() -> new NotFoundException(BusinessErrorCode.NOT_FOUND));
+		return MemberResponse.of(member);
+	}
+
+	public OrganizationResponses getJoinedOrganizations(Long memberId) {
+		return null;
+	}
 }
