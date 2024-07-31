4a6088802396d4c4dfeacaa3de49f7617ff89fa7
Hyoseop Song <crad_on25@naver.com>
2024-07-31 17:14:48 +0900

    :hammer: fix: 사용자 조회 응답 조직목록 출력형식 변경

--- src/main/java/com/notitime/noffice/response/MemberResponse.java
+++ src/main/java/com/notitime/noffice/response/MemberResponse.java
@@ -1,11 +1,17 @@
 package com.notitime.noffice.response;
 
 import com.notitime.noffice.domain.member.model.Member;
+import java.util.List;
 
 public record MemberResponse(Long id, String name, String alias, String profileImage,
-                             OrganizationResponses organizations) {
+                             List<OrganizationResponse> organizations) {
 	public static MemberResponse of(Member member) {
-		return new MemberResponse(member.getId(), member.getName(), member.getAlias(),
-				member.getProfileImage(), OrganizationResponses.from(member.getOrganizations()));
+		return new MemberResponse(
+				member.getId(),
+				member.getName(),
+				member.getAlias(),
+				member.getProfileImage(),
+				OrganizationResponses.from(member.getOrganizations()).organizations()
+		);
 	}
 }
