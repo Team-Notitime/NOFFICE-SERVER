188535b284362a5a60b7e2dfa9c3862800c0a3f6
Hyoseop Song <crad_on25@naver.com>
2024-08-02 22:39:30 +0900

    :sparkles: feat: 노티 대면여부, 투두 목록, 알림시간 기록 테이블 스펙 추가 (#33)

--- src/main/java/com/notitime/noffice/domain/Announcement.java
+++ src/main/java/com/notitime/noffice/domain/Announcement.java
@@ -2,13 +2,17 @@ package com.notitime.noffice.domain;
 
 import com.notitime.noffice.domain.member.model.Member;
 import com.notitime.noffice.domain.organization.model.Organization;
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -29,16 +33,21 @@ public class Announcement extends BaseTimeEntity {
 	@Column(columnDefinition = "TEXT")
 	private String profileImageUrl;
 
+	@Column(nullable = false)
+	private boolean isFaceToFace;
+
 	private String placeLinkName;
 
 	@Column(columnDefinition = "TEXT")
 	private String placeLinkUrl;
 
-	private LocalDateTime startAt;
-
 	private LocalDateTime endAt;
 
-	private LocalDateTime noticeAt;
+	@OneToMany(mappedBy = "announcement", cascade = CascadeType.ALL, orphanRemoval = true)
+	private List<Task> tasks = new ArrayList<>();
+
+	@OneToMany(mappedBy = "announcement", cascade = CascadeType.ALL, orphanRemoval = true)
+	private List<Notification> notifications = new ArrayList<>();
 
 	@ManyToOne(fetch = FetchType.LAZY)
 	@JoinColumn(name = "creator_id")
---
+++ src/main/java/com/notitime/noffice/domain/Notification.java
@@ -0,0 +1,29 @@
+package com.notitime.noffice.domain;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import java.time.LocalDateTime;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+public class Notification {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "announcement_id", nullable = false)
+	private Announcement announcement;
+
+	private LocalDateTime sendAt;
+}
