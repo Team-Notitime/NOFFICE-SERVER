c536cbd9bafc6b36c775706c88938b4ccbe95802
Hyoseop Song <crad_on25@naver.com>
2024-08-02 22:38:28 +0900

    :sparkles: feat: 노티 투두 연관관계 설정 (#33)

--- src/main/java/com/notitime/noffice/domain/Task.java
+++ src/main/java/com/notitime/noffice/domain/Task.java
@@ -1,9 +1,13 @@
 package com.notitime.noffice.domain;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -17,9 +21,10 @@ public class Task {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 
-	private Long announceId;
-
-	private Long organizationId;
-
+	@Column(nullable = false)
 	private String content;
+	
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "announcement_id", nullable = false)
+	private Announcement announcement;
 }
