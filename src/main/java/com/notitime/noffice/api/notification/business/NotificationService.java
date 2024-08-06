441b9f9a7eee0ca7c5988e79ecd1aabf9640f63f
Hyoseop Song <crad_on25@naver.com>
2024-08-06 21:20:15 +0900

    :sparkles: feat: 노티 알림 생성 API Business 구현 (#46)

---
+++ src/main/java/com/notitime/noffice/api/notification/business/NotificationService.java
@@ -0,0 +1,25 @@
+package com.notitime.noffice.api.notification.business;
+
+import com.notitime.noffice.domain.announcement.model.Announcement;
+import com.notitime.noffice.domain.announcement.persistence.AnnouncementRepository;
+import com.notitime.noffice.domain.notification.model.Notification;
+import com.notitime.noffice.domain.notification.persistence.NotificationRepository;
+import com.notitime.noffice.request.AnnouncementCreateRequest;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional
+@RequiredArgsConstructor
+public class NotificationService {
+
+	private final NotificationRepository notificationRepository;
+	private final AnnouncementRepository announcementRepository;
+
+	public void createNotification(AnnouncementCreateRequest request, Announcement announcement) {
+		List<Notification> notifications = NotificationAssembler.assemble(request, announcement);
+		notificationRepository.saveAll(notifications);
+	}
+}
--- src/main/java/com/notitime/noffice/domain/notification/model/Notification.java
+++ src/main/java/com/notitime/noffice/domain/notification/model/Notification.java
@@ -28,10 +28,21 @@ public class Notification {
 
 	@Column(nullable = false)
 	private String content;
-	
+
 	@ManyToOne(fetch = FetchType.LAZY)
 	@JoinColumn(name = "announcement_id", nullable = false)
 	private Announcement announcement;
 
 	private LocalDateTime sendAt;
+
+	private Notification(Announcement announcement, LocalDateTime sendAt) {
+		this.announcement = announcement;
+		this.title = announcement.getTitle();
+		this.content = announcement.getContent();
+		this.sendAt = sendAt;
+	}
+
+	public static Notification createNotification(Announcement announcement, LocalDateTime sendAt) {
+		return new Notification(announcement, sendAt);
+	}
 }
