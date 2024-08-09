5aedf563dc9759988bd90abba930f7a64368e813
Hyoseop Song <crad_on25@naver.com>
2024-08-09 21:20:59 +0900

    :sparkles: feat: 사용자별 열람상태 기준 투두 조회 쿼리 메소드 구현 (#55)

--- src/main/java/com/notitime/noffice/domain/task/model/Task.java
+++ src/main/java/com/notitime/noffice/domain/task/model/Task.java
@@ -1,5 +1,6 @@
 package com.notitime.noffice.domain.task.model;
 
+import com.notitime.noffice.domain.BaseTimeEntity;
 import com.notitime.noffice.domain.announcement.model.Announcement;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
@@ -16,7 +17,7 @@ import lombok.NoArgsConstructor;
 @Entity
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Getter
-public class Task {
+public class Task extends BaseTimeEntity {
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
