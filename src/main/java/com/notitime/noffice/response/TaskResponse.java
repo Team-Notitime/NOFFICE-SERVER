aa265680e1f336aa82cb5a81d537aab067a3b3bc
Hyoseop Song <crad_on25@naver.com>
2024-08-09 21:26:32 +0900

    :sparkles: feat: 사용자 할당 투두 조회 Business 구현 (#55)

--- src/main/java/com/notitime/noffice/response/TaskResponse.java
+++ src/main/java/com/notitime/noffice/response/TaskResponse.java
@@ -1,9 +1,22 @@
 package com.notitime.noffice.response;
 
 import com.notitime.noffice.domain.task.model.Task;
+import com.notitime.noffice.domain.task.model.TaskStatus;
+import java.util.List;
 
 public record TaskResponse(Long id, Long announcementId, String content) {
 	public static TaskResponse from(Task task) {
 		return new TaskResponse(task.getId(), task.getAnnouncement().getId(), task.getContent());
+	}
+
+	public static TaskResponse from(TaskStatus taskStatus) {
+		Task task = taskStatus.getTask();
+		return new TaskResponse(task.getId(), task.getAnnouncement().getId(), task.getContent());
+	}
+
+	public static List<TaskResponse> fromTaskStatus(List<TaskStatus> taskStatuses) {
+		return taskStatuses.stream()
+				.map(TaskResponse::from)
+				.toList();
 	}
 }
