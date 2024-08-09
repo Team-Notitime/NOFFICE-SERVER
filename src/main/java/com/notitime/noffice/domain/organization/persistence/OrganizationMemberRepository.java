87b46271d9cad7cd1f5a9635c209ef3e5d8ee402
Hyoseop Song <crad_on25@naver.com>
2024-08-09 21:21:49 +0900

    :sparkles: feat: 사용자별 조직 목록 페이징 메소드 구현 (#55)

--- src/main/java/com/notitime/noffice/domain/organization/persistence/OrganizationMemberRepository.java
+++ src/main/java/com/notitime/noffice/domain/organization/persistence/OrganizationMemberRepository.java
@@ -1,9 +1,17 @@
 package com.notitime.noffice.domain.organization.persistence;
 
+import com.notitime.noffice.domain.organization.model.Organization;
 import com.notitime.noffice.domain.organization.model.OrganizationMember;
 import java.util.List;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Slice;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface OrganizationMemberRepository extends JpaRepository<OrganizationMember, Long> {
 	List<OrganizationMember> findByMemberId(Long memberId);
+
+	@Query("SELECT om.organization FROM OrganizationMember om WHERE om.member.id = :memberId")
+	Slice<Organization> findOrganizationsByMemberId(@Param("memberId") Long memberId, Pageable pageable);
 }
