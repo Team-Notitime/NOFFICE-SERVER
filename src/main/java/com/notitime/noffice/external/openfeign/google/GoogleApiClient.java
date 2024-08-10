a7d4d09ead315542c63863387677372c0fd48174
Hyoseop Song <crad_on25@naver.com>
2024-08-10 14:02:22 +0900

    :hammer: fix: google feign error decoder 분리 (#59)

--- src/main/java/com/notitime/noffice/external/openfeign/google/GoogleApiClient.java
+++ src/main/java/com/notitime/noffice/external/openfeign/google/GoogleApiClient.java
@@ -5,7 +5,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
 
-@FeignClient(name = "GoogleApiClient", url = "https://www.googleapis.com", configuration = GoogleFeignClientConfiguration.class)
+@FeignClient(name = "GoogleApiClient", url = "https://www.googleapis.com")
 public interface GoogleApiClient {
 
 	@GetMapping("/oauth2/v3/userinfo")
--- src/main/java/com/notitime/noffice/external/openfeign/google/GoogleAuthApiClient.java
+++ src/main/java/com/notitime/noffice/external/openfeign/google/GoogleAuthApiClient.java
@@ -5,7 +5,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-@FeignClient(name = "GoogleAuthApiClient", url = "${client.google-auth.url}", configuration = GoogleFeignClientConfiguration.class)
+@FeignClient(name = "GoogleAuthApiClient", url = "${client.google-auth.url}")
 public interface GoogleAuthApiClient {
 
 	@PostMapping("/token")
