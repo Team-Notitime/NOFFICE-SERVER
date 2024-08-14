66ce98e5ed67f9e5e88d1ee5551e7ec71a8fd65d
Hyoseop Song <crad_on25@naver.com>
2024-08-15 00:10:12 +0900

    :lock: fix: 외부 인가 uri Security Whitelist 구성

--- src/main/java/com/notitime/noffice/global/config/SecurityWhiteListPaths.java
+++ src/main/java/com/notitime/noffice/global/config/SecurityWhiteListPaths.java
@@ -7,30 +7,26 @@ import org.springframework.util.PathMatcher;
 
 public class SecurityWhiteListPaths {
 	private static final PathMatcher pathMatcher = new AntPathMatcher();
-	public static final List<String> FILTER_WHITE_LIST = List.of(
-			"/health",
-			"/api/v1/**",
-			"/swagger-ui/**",
-			"/swagger-resources/**",
-			"/api-docs/**",
-			"/v3/api-docs/**",
-			"/webjars/**",
-			"/h2-console/**"
-	);
-
 	public static final String[] SECURITY_WHITE_LIST = {
 			"/health",
-			"/api/v1/**",
-			"/v1/user/**",
 			"/error",
 			"/swagger-ui/**",
 			"/swagger-resources/**",
+			"/api/v1/member/login",
 			"/api-docs/**",
 			"/v3/api-docs/**",
 			"/webjars/**",
 			"/h2-console/**"
 	};
 
+	public static final List<String> FILTER_WHITE_LIST = List.of(
+			"/api/v1/auth/google/callback",
+			"/swagger-ui/**",
+			"/swagger-resources/**",
+			"/api-docs/**",
+			"/api/v1/member/login"
+	);
+	
 	public static boolean isWhitelisted(HttpServletRequest request) {
 		String path = request.getRequestURI();
 		return FILTER_WHITE_LIST.stream().anyMatch(pattern -> pathMatcher.match(pattern, path));
