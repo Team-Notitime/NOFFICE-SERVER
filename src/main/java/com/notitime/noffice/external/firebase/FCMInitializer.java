108dff13583f47328ee7ae0efd604e2792dc4896
Hyoseop Song <crad_on25@naver.com>
2024-08-19 18:28:27 +0900

    :sparkles: feat: FCM connection initializer 구현 (#47)

---
+++ src/main/java/com/notitime/noffice/external/firebase/FCMInitializer.java
@@ -0,0 +1,39 @@
+package com.notitime.noffice.external.firebase;
+
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseOptions;
+import jakarta.annotation.PostConstruct;
+import java.io.IOException;
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.stereotype.Service;
+
+@Slf4j
+@Service
+public class FCMInitializer {
+
+	private static final Logger logger = LoggerFactory.getLogger(FCMInitializer.class);
+
+	@Value("${firebase.cloud-messaging.server-key}")
+	private String firebaseConfigPath;
+
+	@PostConstruct
+	public void initialize() {
+		try {
+			FirebaseOptions options = new FirebaseOptions.Builder()
+					.setCredentials(
+							GoogleCredentials.fromStream(new ClassPathResource(firebaseConfigPath).getInputStream()))
+					.build();
+			if (FirebaseApp.getApps().isEmpty()) {
+				FirebaseApp.initializeApp(options);
+				logger.info("Firebase application has been initialized");
+			}
+		} catch (IOException e) {
+			logger.error(e.getMessage());
+		}
+	}
+}
