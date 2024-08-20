c8e797962e95cace03864af02805fdf1a556b1d5
Hyoseop Song <crad_on25@naver.com>
2024-08-20 22:13:14 +0900

    :sparkles: feat: 프로모션 검증 In-memory caching 적용 (#89)

--- src/main/java/com/notitime/noffice/api/promotion/business/PromotionCodeVerifier.java
+++ src/main/java/com/notitime/noffice/api/promotion/business/PromotionCodeVerifier.java
@@ -1,11 +1,15 @@
 package com.notitime.noffice.api.promotion.business;
 
+import static com.notitime.noffice.global.response.BusinessErrorCode.NOT_FOUND_PROMOTION;
+
 import com.notitime.noffice.api.promotion.presentation.dto.PromotionVerifyResponse;
 import com.notitime.noffice.domain.promotion.Promotion;
 import com.notitime.noffice.domain.promotion.persistence.PromotionRepository;
 import com.notitime.noffice.global.exception.NotFoundException;
-import com.notitime.noffice.global.response.BusinessErrorCode;
+import com.notitime.noffice.global.util.HashUtil;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -14,9 +18,32 @@ public class PromotionCodeVerifier {
 
 	private final PromotionRepository promotionRepository;
 
+	@Value("${promotion.welcome.code}")
+	private String PromotionCodeOrigin;
+
+	@Value("${promotion.config.min-length}")
+	private int CODE_MIN_LENGTH;
+
+	@Cacheable(value = "promotion", key = "#promotionCode", condition = "@promotionCodeVerifier.isValidCodeFormat(#promotionCode)")
 	public PromotionVerifyResponse verify(String promotionCode) {
+		validateToHashedPromotionCode(promotionCode);
 		Promotion promotion = promotionRepository.findByCode(promotionCode)
-				.orElseThrow(() -> new NotFoundException(BusinessErrorCode.NOT_FOUND_PROMOTION));
+				.orElseThrow(() -> new NotFoundException(NOT_FOUND_PROMOTION));
 		return PromotionVerifyResponse.of(promotion);
+	}
+
+	private void validateToHashedPromotionCode(String promotionCode) {
+		if (promotionCode.length() < CODE_MIN_LENGTH ||
+				!getHashedPromotionCode(promotionCode).equals(getHashedPromotionCode(PromotionCodeOrigin))) {
+			throw new NotFoundException(NOT_FOUND_PROMOTION);
+		}
+	}
+
+	public boolean isValidCodeFormat(String promotionCode) {
+		return promotionCode.length() >= CODE_MIN_LENGTH;
+	}
+
+	private String getHashedPromotionCode(String promotionCode) {
+		return HashUtil.createHash(promotionCode);
 	}
 }
--- src/main/java/com/notitime/noffice/api/promotion/presentation/PromotionController.java
+++ src/main/java/com/notitime/noffice/api/promotion/presentation/PromotionController.java
@@ -1,23 +1,25 @@
 package com.notitime.noffice.api.promotion.presentation;
 
+import static com.notitime.noffice.global.response.BusinessSuccessCode.VERIFY_PROMOTION_CODE_SUCCESS;
+
 import com.notitime.noffice.api.promotion.business.PromotionCodeVerifier;
 import com.notitime.noffice.api.promotion.presentation.dto.PromotionVerifyResponse;
-import com.notitime.noffice.global.response.BusinessSuccessCode;
 import com.notitime.noffice.global.response.NofficeResponse;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/api/v1/promotion")
 public class PromotionController implements PromotionApi {
 
 	private final PromotionCodeVerifier promotionCodeVerifier;
 
-	@GetMapping
-	public NofficeResponse<PromotionVerifyResponse> verifyPromotionCode(@PathVariable String promotionCode) {
-		return NofficeResponse.success(BusinessSuccessCode.VERIFY_PROMOTION_CODE_SUCCESS,
-				promotionCodeVerifier.verify(promotionCode));
+	@PostMapping("/verify")
+	public NofficeResponse<PromotionVerifyResponse> verifyPromotionCode(@RequestBody final String promotionCode) {
+		return NofficeResponse.success(VERIFY_PROMOTION_CODE_SUCCESS, promotionCodeVerifier.verify(promotionCode));
 	}
 }
