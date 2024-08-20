0100c5f99c8ed2f33ca5ab4a1c4d5fc42826a535
Hyoseop Song <crad_on25@naver.com>
2024-08-20 22:23:20 +0900

    :sparkles: feat: 프로모션 검증 Get Method re-mapping (#89)

--- src/main/java/com/notitime/noffice/api/promotion/presentation/PromotionController.java
+++ src/main/java/com/notitime/noffice/api/promotion/presentation/PromotionController.java
@@ -6,7 +6,7 @@ import com.notitime.noffice.api.promotion.business.PromotionCodeVerifier;
 import com.notitime.noffice.api.promotion.presentation.dto.PromotionVerifyResponse;
 import com.notitime.noffice.global.response.NofficeResponse;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,7 +18,7 @@ public class PromotionController implements PromotionApi {
 
 	private final PromotionCodeVerifier promotionCodeVerifier;
 
-	@PostMapping("/verify")
+	@GetMapping("/verify")
 	public NofficeResponse<PromotionVerifyResponse> verifyPromotionCode(@RequestBody final String promotionCode) {
 		return NofficeResponse.success(VERIFY_PROMOTION_CODE_SUCCESS, promotionCodeVerifier.verify(promotionCode));
 	}
