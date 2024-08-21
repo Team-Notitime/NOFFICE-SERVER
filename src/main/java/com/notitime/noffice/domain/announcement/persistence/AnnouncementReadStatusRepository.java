810c54a18a1b220f1576d031ae8e05b4a5690536
Hyoseop Song <crad_on25@naver.com>
2024-08-22 03:06:41 +0900

    :wrench: fix: 열람자 조회 쿼리 응답 반환 로직 수정 (#121)

--- src/main/java/com/notitime/noffice/domain/announcement/persistence/AnnouncementReadStatusRepository.java
+++ src/main/java/com/notitime/noffice/domain/announcement/persistence/AnnouncementReadStatusRepository.java
@@ -1,6 +1,7 @@
 package com.notitime.noffice.domain.announcement.persistence;
 
 import com.notitime.noffice.domain.announcement.model.AnnouncementReadStatus;
+import com.notitime.noffice.domain.member.model.Member;
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
@@ -15,19 +16,12 @@ public interface AnnouncementReadStatusRepository extends JpaRepository<Announce
 	List<Long> findUnreadMemberIds(Long announcementId);
 
 	@Query("SELECT ars.member FROM AnnouncementReadStatus ars WHERE ars.announcement.id = :announcementId AND ars.isRead = true")
-	List<AnnouncementReadStatus> findReadMembers(@Param("announcementId") Long announcementId);
+	List<Member> findReadMembers(@Param("announcementId") Long announcementId);
 
 	@Query("SELECT ars.member FROM AnnouncementReadStatus ars WHERE ars.announcement.id = :announcementId AND ars.isRead = false")
-	List<AnnouncementReadStatus> findUnReadMembers(@Param("announcementId") Long announcementId);
+	List<Member> findUnReadMembers(@Param("announcementId") Long announcementId);
 
 	List<AnnouncementReadStatus> findByAnnouncementId(Long announcementId);
 
-	/**
-	 * 특정 공지에 대한 열람 상태를 조회합니다.
-	 *
-	 * @param announcementId 공지 ID
-	 * @param memberId       회원 ID
-	 * @return 열람 상태
-	 */
 	AnnouncementReadStatus findByAnnouncementIdAndMemberId(Long announcementId, Long memberId);
 }
