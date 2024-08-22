ee1c92c3fbf5bd4c560ed75ba94300852de58f49
Hyoseop Song <crad_on25@naver.com>
2024-08-22 14:20:39 +0900

    :wrench: fix: 서비스 단위 트랜잭션 전역 설정 (#121)

--- src/main/java/com/notitime/noffice/api/image/strategy/PromotionImageRetrievalStrategy.java
+++ src/main/java/com/notitime/noffice/api/image/strategy/PromotionImageRetrievalStrategy.java
@@ -8,8 +8,10 @@ import com.notitime.noffice.domain.promotion.persistence.PromotionImageRepositor
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
+@Transactional
 @RequiredArgsConstructor
 public class PromotionImageRetrievalStrategy implements ImageRetrievalStrategy<PromotionImage> {
 
--- src/main/java/com/notitime/noffice/api/promotion/business/PromotionCodeVerifier.java
+++ src/main/java/com/notitime/noffice/api/promotion/business/PromotionCodeVerifier.java
@@ -11,8 +11,10 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
+@Transactional
 @RequiredArgsConstructor
 public class PromotionCodeVerifier {
 
