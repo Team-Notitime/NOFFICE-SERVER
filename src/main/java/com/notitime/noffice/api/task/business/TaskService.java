772914226842f0f6cdb1d6bf348ad4d697e1d95f
Hyoseop Song <crad_on25@naver.com>
2024-08-23 01:58:34 +0900

    :recycle: refactor: 조직별 최신 생성 5개 투두만 조회하도록 변경 (#124)

--- src/main/java/com/notitime/noffice/api/task/business/TaskService.java
+++ src/main/java/com/notitime/noffice/api/task/business/TaskService.java
@@ -3,19 +3,18 @@ package com.notitime.noffice.api.task.business;
 import static com.notitime.noffice.global.web.BusinessErrorCode.NOT_FOUND_ANNOUNCEMENT;
 import static com.notitime.noffice.global.web.BusinessErrorCode.NOT_FOUND_TASK;
 
-import com.notitime.noffice.domain.announcement.persistence.AnnouncementRepository;
-import com.notitime.noffice.domain.organization.model.Organization;
-import com.notitime.noffice.domain.organization.persistence.OrganizationMemberRepository;
-import com.notitime.noffice.domain.task.model.Task;
-import com.notitime.noffice.domain.task.model.TaskStatus;
-import com.notitime.noffice.domain.task.persistence.TaskRepository;
-import com.notitime.noffice.domain.task.persistence.TaskStatusRepository;
-import com.notitime.noffice.global.exception.NotFoundException;
 import com.notitime.noffice.api.task.presentation.dto.request.TaskModifyRequest;
 import com.notitime.noffice.api.task.presentation.dto.response.AssignedTaskResponse;
 import com.notitime.noffice.api.task.presentation.dto.response.TaskModifyResponse;
 import com.notitime.noffice.api.task.presentation.dto.response.TaskResponse;
 import com.notitime.noffice.api.task.presentation.dto.response.TaskResponses;
+import com.notitime.noffice.domain.announcement.persistence.AnnouncementRepository;
+import com.notitime.noffice.domain.organization.model.Organization;
+import com.notitime.noffice.domain.organization.persistence.OrganizationMemberRepository;
+import com.notitime.noffice.domain.task.model.Task;
+import com.notitime.noffice.domain.task.persistence.TaskRepository;
+import com.notitime.noffice.global.exception.NotFoundException;
+import java.util.Comparator;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.PageImpl;
@@ -29,7 +28,6 @@ import org.springframework.transaction.annotation.Transactional;
 @RequiredArgsConstructor
 public class TaskService {
 
-	private final TaskStatusRepository taskStatusRepository;
 	private final OrganizationMemberRepository organizationMemberRepository;
 	private final TaskRepository taskRepository;
 	private final AnnouncementRepository announcementRepository;
@@ -41,9 +39,8 @@ public class TaskService {
 
 	public Slice<AssignedTaskResponse> getAssignedTasks(Long memberId, Pageable pageable) {
 		Slice<Organization> organizations = getSlicedOrganizations(memberId, pageable);
-		List<AssignedTaskResponse> responses = organizations.stream()
-				.map(organization -> searchAssignedTasks(memberId, organization))
-				.toList();
+		List<TaskResponse> taskResponses = getTop5LatestTaskResponses(organizations);
+		List<AssignedTaskResponse> responses = assembleTasks(organizations, taskResponses);
 		return new PageImpl<>(responses, pageable, organizations.getSize());
 	}
 
@@ -57,27 +54,35 @@ public class TaskService {
 		taskRepository.deleteById(taskId);
 	}
 
-	private Slice<Organization> getSlicedOrganizations(Long memberId, Pageable pageable) {
-		return organizationMemberRepository.findOrganizationsByMemberId(memberId, pageable);
-	}
-
 	private Task findById(Long taskId) {
 		return taskRepository.findById(taskId)
 				.orElseThrow(() -> new NotFoundException(NOT_FOUND_TASK));
-	}
-
-	private AssignedTaskResponse searchAssignedTasks(Long memberId, Organization organization) {
-		List<TaskResponse> tasks = TaskResponse.fromTaskStatus(getTaskStatuses(memberId, organization.getId()));
-		return new AssignedTaskResponse(organization.getId(), organization.getName(), tasks);
-	}
-
-	private List<TaskStatus> getTaskStatuses(Long memberId, Long organizationId) {
-		return taskStatusRepository.findTop5ByOrganizationIdAndMemberId(organizationId, memberId);
 	}
 
 	private void validateAnnouncement(Long announcementId) {
 		if (!announcementRepository.existsById(announcementId)) {
 			throw new NotFoundException(NOT_FOUND_ANNOUNCEMENT);
 		}
+	}
+
+	private Slice<Organization> getSlicedOrganizations(Long memberId, Pageable pageable) {
+		return organizationMemberRepository.findOrganizationsByMemberId(memberId, pageable);
+	}
+
+	private List<TaskResponse> getTop5LatestTaskResponses(Slice<Organization> organizations) {
+		return organizations.stream()
+				.flatMap(org -> org.getAnnouncements().stream())
+				.flatMap(announcement -> announcement.getTasks().stream())
+				.sorted(Comparator.comparing(Task::getCreatedAt).reversed())
+				.limit(5)
+				.map(TaskResponse::from)
+				.toList();
+	}
+
+	private List<AssignedTaskResponse> assembleTasks(Slice<Organization> organizations,
+	                                                 List<TaskResponse> taskResponses) {
+		return organizations.stream()
+				.map(organization -> AssignedTaskResponse.from(organization, taskResponses))
+				.toList();
 	}
 }
