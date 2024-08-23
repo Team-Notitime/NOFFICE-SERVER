287bfe8a27abadb28f6f55490c1d250314a571c7
Hyoseop Song <crad_on25@naver.com>
2024-08-24 01:29:58 +0900

    :memo: docs: Swagger error response case 기술 (#127)

--- src/main/java/com/notitime/noffice/api/category/presentation/CategoryApi.java
+++ src/main/java/com/notitime/noffice/api/category/presentation/CategoryApi.java
@@ -1,8 +1,10 @@
 package com.notitime.noffice.api.category.presentation;
 
-import com.notitime.noffice.global.web.NofficeResponse;
 import com.notitime.noffice.api.category.presentation.dto.response.CategoryResponses;
+import com.notitime.noffice.global.web.NofficeResponse;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
 
@@ -11,7 +13,7 @@ public interface CategoryApi {
 
 	@Operation(summary = "전체 카테고리 조회", description = "등록 가능한 모든 카테고리를 조회합니다.", responses = {
 			@ApiResponse(responseCode = "200", description = "카테고리 목록 조회 성공"),
-			@ApiResponse(responseCode = "404", description = "등록된 카테고리가 없습니다.")
+			@ApiResponse(responseCode = "404", description = "등록된 카테고리가 없습니다.", content = @Content(schema = @Schema(implementation = NofficeResponse.class))),
 	})
 	NofficeResponse<CategoryResponses> getCategories();
 }
