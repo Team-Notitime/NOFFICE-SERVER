14c9ac5a571569a65c7ad18817e49700b2425495
Hyoseop Song <crad_on25@naver.com>
2024-08-25 11:04:35 +0900

    :recycle: refactor: 공지 미열람자는 전체 멤버의 열람자를 제외함과 동일하다 (#115)

--- src/main/java/com/notitime/noffice/api/announcement/business/ReadStatusRecoder.java
+++ src/main/java/com/notitime/noffice/api/announcement/business/ReadStatusRecoder.java
@@ -1,11 +1,16 @@
 package com.notitime.noffice.api.announcement.business;
 
+import static com.notitime.noffice.global.web.BusinessErrorCode.NOT_FOUND_ORGANIZATION;
+
 import com.notitime.noffice.api.organization.business.RoleVerifier;
+import com.notitime.noffice.domain.JoinStatus;
 import com.notitime.noffice.domain.announcement.model.Announcement;
 import com.notitime.noffice.domain.announcement.model.AnnouncementReadStatus;
 import com.notitime.noffice.domain.announcement.persistence.AnnouncementReadStatusRepository;
 import com.notitime.noffice.domain.announcement.persistence.AnnouncementRepository;
 import com.notitime.noffice.domain.member.model.Member;
+import com.notitime.noffice.domain.organization.model.Organization;
+import com.notitime.noffice.global.exception.NotFoundException;
 import java.time.LocalDateTime;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
@@ -35,7 +40,13 @@ public class ReadStatusRecoder {
 	}
 
 	public List<Member> findUnReadMembers(Long announcementId) {
-		return announcementReadStatusRepository.findUnReadMembers(announcementId);
+		Organization organization = announcementRepository.findById(announcementId)
+				.orElseThrow(() -> new NotFoundException(NOT_FOUND_ORGANIZATION))
+				.getOrganization();
+		List<Member> allMembers = organization.getMembersByStatus(JoinStatus.ACTIVE);
+		List<Member> readMembers = findReadMembers(announcementId);
+		allMembers.removeAll(readMembers);
+		return allMembers;
 	}
 
 	public Long countReader(Long announcementId) {
